417887eb37c4de8967b5f674fc27f7ff779a9165
Aishwarya Shilpi <aishwaryashilpi5395@gmail.com>
2021-08-05 07:25:09 +0000

    personal quest

---
+++ main.py
@@ -0,0 +1,47 @@
+import datetime
+import random
+
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    random_number = random.randint(1, 10)
+    date = datetime.datetime.now()
+    current_year = date.year
+    return render_template('index.html', num=random_number, year=current_year)
+
+@app.route('/guess/<name>')
+def guess(name):
+
+    gender_url = f'https://api.genderize.io/?name={name}'
+    gender_response  = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender  = gender_data["gender"]
+
+    age_url = f'https://api.agify.io/?name={name}'
+    age_response  = requests.get(age_url)
+    age_data = age_response.json()
+    age  = age_data["age"]
+
+    current_year = datetime.datetime.now().year
+
+    return render_template('guess.html', name=name, gender=gender, age=age, year=current_year)
+
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/92e41515e8e7f8466117"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+
+    current_year = datetime.datetime.now().year
+
+    return render_template('blog.html', posts = all_posts, year=current_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
